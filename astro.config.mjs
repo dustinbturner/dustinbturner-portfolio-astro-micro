d878d7c179d78385836fb98717dbaebd69adfcf1
Dustin Turner <dustin@dustinbturner.com>
2024-08-13 18:31:33 -0400

    analytics and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/static";
+
+
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
@@ -8,6 +11,15 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    speedInsights: {
+      enabled: true
+    }
+  }),
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
